3745811b28ffbe1e5f32748253539b2fb98c0b6b
ricardo <ricardo.sikic@gmail.com>
2019-10-15 18:56:34 -0300

    agregados ultimos metodos y materia

--- clase4-maths&numbers/ex1.rb
+++ clase4-maths&numbers/ex1.rb
@@ -24,6 +24,7 @@ puts 8 % 3
 puts 5 % 5 
 puts 10 % 4
 puts 5 % 2
+puts 16 % 5
 
 # otro metodo es el de abs() lo que hace este metodo es mostrar solo valores absolutos
 # positivos
@@ -33,3 +34,37 @@ puts num.abs()
 
 num_dos = -100
 puts num_dos.abs()
+
+# existen otros metodos para redondear numeros del tipo flotante como .round()
+# este redondeara hacia arriba si toma el 5 y bajo el decimal 5 redondea hacia a abajo
+
+flotante_uno = 4.55
+puts flotante_uno.round()
+
+flotante_dos = 4.45
+puts flotante_dos.round()
+
+
+# otros metodos de redondeo, es el .ceil() que redondea hacia arriba y .floor()
+# que lo hace hacia abajo
+
+numero_ceil = 4.4
+puts numero_ceil.ceil()
+
+numero_floor = 3.55
+puts numero_floor.floor()
+
+# hay otras operaciones matematicas como las que usan la funcion Math un ejemplo son
+# Math.sqrt(), Math.log()
+
+puts Math.sqrt(8)
+puts Math.log(1)
+
+
+# ejercicios miscelaneos
+numer = -40 
+puts numer.abs()
+
+a = 40
+puts Math.sqrt(a)
+
