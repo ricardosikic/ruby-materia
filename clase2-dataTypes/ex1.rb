358195380b78c7f7788178752b9bb3a323232327
ricardo <ricardo.sikic@gmail.com>
2019-10-15 15:11:09 -0300

    clase 2 data types

---
+++ clase2-dataTypes/ex1.rb
@@ -0,0 +1,30 @@
+# TIPOS DE DATOS EN RUBY
+# strings
+variable_uno = "esto es un string"
+variable_dos = "esto es otro string"
+
+# numericos
+# nota que no utilizo ""
+edad = 31
+numero_autos = 3
+
+# numeros decimales o floats
+
+area = 34.5
+primedio_notas = 5.50
+
+puts "la casa tiene un area de " + area.to_s 
+
+
+#numeros booleanos
+
+alto = false
+asistencia_evento = true
+
+puts asistencia_evento
+
+# tipo de dato nil
+
+# basicamente significa que no hay un valor asignado a la variable
+numero = nil
+puts numero
