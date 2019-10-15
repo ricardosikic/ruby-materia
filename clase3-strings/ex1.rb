81bbfec2a65fdc35c6cbf3eeacb31724f87bc0a2
ricardo <ricardo.sikic@gmail.com>
2019-10-15 15:24:44 -0300

    clase 3 faltan otros metodos

---
+++ clase3-strings/ex1.rb
@@ -0,0 +1,20 @@
+# clase de strings y algunos metodos 
+phrase = "clase de strings"
+puts phrase
+
+# ahora utilizaremos el metodo upcase() / downcase() para transformar
+# el string en letras mayusculas o minusculas
+puts phrase.upcase()
+puts phrase.downcase()
+
+# metodo strip, lo que hace este metodo strip() para eliminar los
+# espacios dentro de un string
+phrase_dos = "   este es un texto con espacio   "
+puts phrase_dos
+puts phrase_dos.strip()
+
+# metodo para saber el largo de un string
+# utilizando el metodo length()
+
+phrase_tres = 'hola este es el texto'
+puts phrase_tres.length() 
