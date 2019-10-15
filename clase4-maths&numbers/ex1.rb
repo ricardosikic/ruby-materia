723bebd99361df85266d55bded43e45aa8dd13ed
ricardo <ricardo.sikic@gmail.com>
2019-10-15 18:42:43 -0300

    clase 4 math & numbers faltan methods

---
+++ clase4-maths&numbers/ex1.rb
@@ -0,0 +1,35 @@
+# las operaciones matematicas basicas para ruby son
+# suma, resta, multiplicacion y division, ej:
+
+puts 4 + 4
+puts 4 - 3
+puts 3 - 4
+puts 2 * 2
+puts 2 / 2
+
+# como ves ruby tambien treabaja con numeros negativos.
+
+# ruby tambien trabaja con operaciones en los que hay un exponente
+
+puts 2**3
+puts 2**2
+puts 4**5
+
+# otra operacion matematica que se usa en rb son los % (modulos), lo que hacen
+# es dividir un numero con otro y entregar el resto.
+
+puts 5 % 4
+puts 10 % 5
+puts 8 % 3
+puts 5 % 5 
+puts 10 % 4
+puts 5 % 2
+
+# otro metodo es el de abs() lo que hace este metodo es mostrar solo valores absolutos
+# positivos
+
+num = -20
+puts num.abs()
+
+num_dos = -100
+puts num_dos.abs()
