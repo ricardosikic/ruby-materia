d6860cdb31b27e855118c2ff245a4fccc7e79223
ricardo <ricardo.sikic@gmail.com>
2019-10-15 14:55:57 -0300

    agregado ejercicio a clase

--- clase1-variables/ex1.rb
+++ clase1-variables/ex1.rb
@@ -1,6 +1,12 @@
-# declaro variables
-nombre_animal = "elefante"
+# # declaro variables
+# nombre_animal = "elefante"
+# tipo_animal = "vertebrado"
 
+# puts "el " + nombre_animal + " es muy grande"
+# puts "que pertenece a los " + tipo_animal + " mamiferos"
 
-puts "el elefante es muy grande"
-puts "que pertenece a los vertebrados mamiferos"
+nombre_persona = "ricardo"
+edad_persona = 31
+
+puts "me llamo " + nombre_persona
+puts "tengo la de edad de " + edad_persona.to_s + " años"
