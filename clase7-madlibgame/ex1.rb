d182632b7c847fdfa40c09b3c780719339b3ad78
ricardo <ricardo.sikic@gmail.com>
2019-10-15 21:12:52 -0300

    clase 7 juego de frases subido

---
+++ clase7-madlibgame/ex1.rb
@@ -0,0 +1,14 @@
+puts 'ingresa un color'
+color_rosas = gets.chomp()
+
+puts 'ingresa tipo de flores'
+nombre_flores = gets.chomp()
+
+puts 'ingresa nombre de famoso'
+nombre_celebridad = gets.chomp()
+
+
+
+puts ('las rosas son ' + color_rosas)
+puts (nombre_flores + ' azules')
+puts ('te quiero ' + nombre_celebridad)
