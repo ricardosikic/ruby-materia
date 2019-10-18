b40621daf8e89185f581a0af14b26d45fd4b7ac5
ricardo <ricardo.sikic@gmail.com>
2019-10-17 21:08:45 -0300

    agregada calculadora con clase

--- clase21-teoriaClasesOb/ex1.rb
+++ clase21-teoriaClasesOb/ex1.rb
@@ -105,3 +105,24 @@ end
 
 auto1 = Auto.new('toyota', 2019)
 puts auto1.info_auto
+
+
+class Calculadora
+    attr_accessor :num1, :num2
+
+    def initialize(num1, num2)
+        @num1 = num1
+        @num2 = num2
+    end
+
+    def suma
+        return @num1 + @num2
+    end
+
+
+end
+
+calculadora1 = Calculadora.new(2, 2)
+
+puts calculadora1.suma
+
